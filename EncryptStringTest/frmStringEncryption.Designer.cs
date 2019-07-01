384d7f7993f5a90c53b53279cca0c3957dc9d844
Isaac Be <esaaco@gmail.com>
2019-07-01 21:41:15 +0200

    Add files via upload

---
+++ EncryptStringTest/frmStringEncryption.Designer.cs
@@ -0,0 +1,147 @@
+﻿namespace EncryptStringTest
+{
+    partial class frmStringEncryption
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBoxString = new System.Windows.Forms.TextBox();
+            this.textBoxEncrypted = new System.Windows.Forms.TextBox();
+            this.butEncrypt = new System.Windows.Forms.Button();
+            this.butDecrypt = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPassword = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "String";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(13, 118);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Encrypted";
+            // 
+            // textBoxString
+            // 
+            this.textBoxString.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxString.Location = new System.Drawing.Point(68, 13);
+            this.textBoxString.Name = "textBoxString";
+            this.textBoxString.Size = new System.Drawing.Size(351, 20);
+            this.textBoxString.TabIndex = 1;
+            // 
+            // textBoxEncrypted
+            // 
+            this.textBoxEncrypted.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxEncrypted.Location = new System.Drawing.Point(68, 111);
+            this.textBoxEncrypted.Name = "textBoxEncrypted";
+            this.textBoxEncrypted.Size = new System.Drawing.Size(351, 20);
+            this.textBoxEncrypted.TabIndex = 7;
+            // 
+            // butEncrypt
+            // 
+            this.butEncrypt.Location = new System.Drawing.Point(8, 82);
+            this.butEncrypt.Name = "butEncrypt";
+            this.butEncrypt.Size = new System.Drawing.Size(60, 23);
+            this.butEncrypt.TabIndex = 4;
+            this.butEncrypt.Text = "Encrypt";
+            this.butEncrypt.UseVisualStyleBackColor = true;
+            this.butEncrypt.Click += new System.EventHandler(this.butEncrypt_Click);
+            // 
+            // butDecrypt
+            // 
+            this.butDecrypt.Location = new System.Drawing.Point(74, 82);
+            this.butDecrypt.Name = "butDecrypt";
+            this.butDecrypt.Size = new System.Drawing.Size(60, 23);
+            this.butDecrypt.TabIndex = 5;
+            this.butDecrypt.Text = "Decrypt";
+            this.butDecrypt.UseVisualStyleBackColor = true;
+            this.butDecrypt.Click += new System.EventHandler(this.butDecrypt_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 51);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(53, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Password";
+            // 
+            // textBoxPassword
+            // 
+            this.textBoxPassword.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxPassword.Location = new System.Drawing.Point(68, 48);
+            this.textBoxPassword.Name = "textBoxPassword";
+            this.textBoxPassword.Size = new System.Drawing.Size(351, 20);
+            this.textBoxPassword.TabIndex = 3;
+            // 
+            // frmStringEncryption
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(431, 151);
+            this.Controls.Add(this.textBoxPassword);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.butDecrypt);
+            this.Controls.Add(this.butEncrypt);
+            this.Controls.Add(this.textBoxEncrypted);
+            this.Controls.Add(this.textBoxString);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmStringEncryption";
+            this.Text = "String Encryption";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBoxString;
+        private System.Windows.Forms.TextBox textBoxEncrypted;
+        private System.Windows.Forms.Button butEncrypt;
+        private System.Windows.Forms.Button butDecrypt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPassword;
+    }
+}
+
---
+++ EncryptStringTest/frmStringEncryption.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EncryptStringTest
+{
+    public partial class frmStringEncryption : Form
+    {
+        public frmStringEncryption()
+        {
+            InitializeComponent();
+        }
+
+        private void butEncrypt_Click(object sender, EventArgs e)
+        {
+            if ((!string.IsNullOrWhiteSpace(textBoxString.Text) && !string.IsNullOrWhiteSpace(textBoxString.Text))==true)
+            {
+                try
+                {
+                    textBoxEncrypted.Text = Encrypt.EncryptString(textBoxString.Text, textBoxPassword.Text);
+                    textBoxString.Text = "";
+                }
+                catch( Exception ex )
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+
+        private void butDecrypt_Click(object sender, EventArgs e)
+        {
+            if ((!string.IsNullOrWhiteSpace(textBoxEncrypted.Text) && !string.IsNullOrWhiteSpace(textBoxPassword.Text))==true)
+            {
+                try
+                {
+                    textBoxString.Text = Encrypt.DecryptString(textBoxEncrypted.Text, textBoxPassword.Text);
+                    textBoxEncrypted.Text = "";
+                }
+                catch( Exception ex )
+                {
+                    MessageBox.Show(ex.ToString());
+                }
+            }
+        }
+    }
+}
